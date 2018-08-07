3b70d21f211242b8b47e312223083414b6b6c7cf
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-07 16:37:27 -0500

    React: Add styles to the project with Radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Radium from 'radium';
+
 import './Person.css';
+
 
 const person = (props) => {
 
@@ -12,4 +15,4 @@ const person = (props) => {
   );
 }
 
-export default person;
+export default Radium(person);
